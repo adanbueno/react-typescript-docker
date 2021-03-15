d05f9c40841aa18fe10f358f053254b110a9835d
adanbueno <adan_bueno@hotmail.com>
2021-03-15 14:25:13 -0300

    Feat: Incrementando TodoList

---
+++ README.md
@@ -0,0 +1,13 @@
+
+## Descrição
+
+Projeto  TODO- LIST básico em react, usando typescript e aplicando os conceitos de docker
+
+## Rodar a aplicação
+
+```
+docker-compose up
+```
+
+Acessar http://localhost:3000 no browser.
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { FormEvent, useState } from "react";
+import { Header } from "./components/Header";
+import { TextField } from "./components/TextField";
 
+interface Task {
+  name: string;
+  description: string;
+}
+//JSX
+//F(X) => HTML
 function App() {
+  const [name, setName] = useState(""); //React Hooks
+  const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]); //Generic
+
+  function onSubmit(event: FormEvent) {
+    event.preventDefault();
+    setTasks([...tasks, { name, description }]);
+  }
+
   return (
     <div>
-      <h1>Todo List</h1>
+      <Header />
+      <form onSubmit={onSubmit}>
+        <TextField
+          id="name"
+          label="Nome"
+          onChange={(value) => setName(value)}
+        />
+        <TextField
+          id="description"
+          label="Descrição"
+          onChange={(value) => setDescription(value)}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li>
+            {task.name} - {task.description}
+          </li>
+        ))}
+      </ul>
     </div>
-
   );
 }
 
 export default App;
 
-//Paradigma orientado a componentes
+//Usar o paradigma orientado a componentes
